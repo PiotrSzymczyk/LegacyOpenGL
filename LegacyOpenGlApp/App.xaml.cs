ecf317f7a724068c0a53e5a29aadf991369db8fa
Piotr Szymczyk <ja.piotrek.szymczyk@gmail.com>
2017-11-03 00:18:26 +0100

    register model repository as singleton & change current culture number formatting

--- LegacyOpenGlApp/App.xaml.cs
+++ LegacyOpenGlApp/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Globalization;
+using System.Windows;
+using LegacyOpenGlApp.DataAccess.Services;
 using LegacyOpenGlApp.WindowModels;
 using Unity;
 
@@ -11,8 +13,14 @@ namespace LegacyOpenGlApp
 	{
 		protected override void OnStartup(StartupEventArgs e)
 		{
+			CultureInfo customCulture = (CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+			customCulture.NumberFormat.NumberDecimalSeparator = ".";
+
+			System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+
 			IUnityContainer container = new UnityContainer();
 
+			container.RegisterType<ModelRepositoryService>(new ContainerControlledLifetimeManager());
 			MainWindow mainWindow = container.Resolve<MainWindow>();
 			mainWindow.Show();
 		}
